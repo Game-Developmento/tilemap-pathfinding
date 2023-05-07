550252f5d30127c5ca0d48a779b5f9f239abd558
oriHoward <ori.howard@gmail.com>
2023-05-07 12:29:32 +0300

    Added new scene , started working on the dijkstra algo

---
+++ Assets/Scripts/ShortestPath/newChaser.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newChaser : TargetMoverDijkstra
+{
+    [Tooltip("The object that we try to chase")]
+    [SerializeField] Transform targetObject = null;
+
+    public Vector3 TargetObjectPosition()
+    {
+        return targetObject.position;
+    }
+
+    private void Update()
+    {
+        SetTarget(Vector3Int.RoundToInt(targetObject.position));
+    }
+}
